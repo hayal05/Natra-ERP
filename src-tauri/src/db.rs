28137884102585e03a55b0e04a521474ab9184b5
hayal05 <hayelommengistu@gmail.com>
2026-08-19 20:44:00 +0300

    Fix default admin password seed hash

--- src-tauri/src/db.rs
+++ src-tauri/src/db.rs
@@ -7,7 +7,7 @@ use std::{
 use thiserror::Error;
 
 pub const DB_SCHEMA_VERSION: i32 = 12;
-const DEFAULT_ADMIN_PASSWORD_HASH: &str = "$argon2id$v=19$m=65536,t=3,p=4$9+8DePK/MlZJ/0iA2XHylg$jVFn51IEt/eYTkue7hkmbJJlfg1mxsksIV3NwWFxilE";
+const DEFAULT_ADMIN_PASSWORD_HASH: &str = "$argon2id$v=19$m=65536,t=3,p=4$nuDPZRiwVbRM7K+pXEXq5g$HzsV3AgEt2naxLZRhmwX4n6hEfP0XnNAYF05Lg/ALXY";
 
 #[derive(Debug, Error)]
 pub enum DbError {
